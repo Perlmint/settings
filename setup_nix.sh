14a41af34459eb22653d69be80fc3b86b9f544b8
Gyusun Yeom <omniavinco@gmail.com>
2016-08-13 07:28:18 +0900

    Updats setup_nix.sh - zsh

--- setup_nix.sh
+++ setup_nix.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 THISDIR=$(dirname "$PWD/$0")
+
 ln -s $THISDIR/.emacs.d $HOME/.emacs.d
+
+# install oh-my-zsh & setup zshrc
+[ ! -d $HOME/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+[ -f $HOME/.zshrc ] && rm $HOME/.zshrc
+ln -s %THISDIR/zsh/.zshrc $HOME/.zshrc
