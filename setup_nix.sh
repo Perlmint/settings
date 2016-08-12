e1bd7f22b0f86aee0c1bfa46c01416726d10b3e0
Gyusun Yeom <omniavinco@gmail.com>
2016-08-13 07:31:27 +0900

    fix mistype

--- setup_nix.sh
+++ setup_nix.sh
@@ -7,4 +7,4 @@ ln -s $THISDIR/.emacs.d $HOME/.emacs.d
 # install oh-my-zsh & setup zshrc
 [ ! -d $HOME/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 [ -f $HOME/.zshrc ] && rm $HOME/.zshrc
-ln -s %THISDIR/zsh/.zshrc $HOME/.zshrc
+ln -s $THISDIR/zsh/.zshrc $HOME/.zshrc
