f85b1ff6ac3bba6f069fc35ecf9ab157ae399572
Gyusun Yeom <omniavinco@gmail.com>
2017-02-13 12:50:56 +0900

    create link to appdata/roaming for windows default

--- setup_win_impl.cmd
+++ setup_win_impl.cmd
@@ -1,4 +1,5 @@
 SET thispath=%~dp0
 
 mklink /d %USERPROFILE%\.emacs.d %thispath%\.emacs.d
+mklink /d %USERPROFILE%\AppData\Roaming\.emacs.d %thispath%\.emacs.d
 mklink %APPDATA%\ConEmu.xml %thispath%\ConEmu.xml
